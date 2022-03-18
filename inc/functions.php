6be551fdaa2b7d20198844f6699f40b00b73657c
jymyte <jyrimy@gmail.com>
2022-03-18 21:30:44 +0200

    Working backend

---
+++ inc/functions.php
@@ -0,0 +1,14 @@
+<?php 
+function openDB() {
+  $db = new PDO('mysql:host=localhost;dbname=shoppinglist;charset=utf8', 'root');
+  $db -> setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+  return $db;
+}
+
+function returnError(PDOException $pdoex) {
+  echo header('HTTP/1.1 500 Interna Server Error');
+  $error = array('error' => $pdoex->getMessage());
+  print json_encode($error);
+}
+
+?>
